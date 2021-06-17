d22ca7ffa7cb17ccbba8c4cf15e6a5071ffc80a7
João Pedro Barboza Garcia - JGA <joao.garcia@iteris.com.br>
2021-06-17 12:24:59 -0300

    feat: escopo

--- escopo.js
+++ escopo.js
@@ -1,7 +1,31 @@
-const escopoJs = function() {
-    console.group('Escopo');
-    console.log(this);
-    console.groupEnd('Escopo');
-}
+/*! *****************************************************************************
+O código abaixo tem como objetivo demonstrar que a palavra 'this' se refere ao
+contexto de execução do JavaScript.
 
+Dicas e sugestões são sempre bem vindas!!!
+
+***************************************************************************** */
+
+console.log('Inicio da execução');
+console.log('===============================');
+
+const escopoJs = function () {
+  console.log('Escopo:', this);
+  console.log('===============================');
+};
+
+/**
+ * Chamada simples: Contexto permanece
+ */
 escopoJs();
+
+/**
+ * Chamada call: Contexto é alterado
+ */
+escopoJs.call({});
+
+/**
+ * Chamada através do new: Contexto também é alterado
+ */
+new escopoJs();
+
